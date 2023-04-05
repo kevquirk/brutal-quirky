12ced2e6cd85d2ec28fb53d39722e496a8a5540e
Kev Quirk <kev@quirk.cc>
2023-04-05 09:12:42 +0100

    update

--- functions.php
+++ functions.php
@@ -77,11 +77,4 @@ add_filter('default_title', function ($title) {
         return date('d F Y H:i');
     }
     return $title;
-}
-
-// Add date & time to posts
-add_filter( 'get_comment_date', 'db_reformat_comment_date' );	
-function db_reformat_comment_date'( $dateForm ) {
-    $dateForm  = date("j F Y H:i");	
-    return $dateForm;
-}
+} );
