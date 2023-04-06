109b2ef57f16baaa5d5d8b99eafc3b15d874d93d
Kev Quirk <kev@quirk.cc>
2023-04-06 15:47:14 +0100

    404

--- 404.php
+++ 404.php
@@ -14,7 +14,10 @@ get_header(); ?>
 	<div <?php generate_do_attr( 'content' ); ?>>
 		<main <?php generate_do_attr( 'main' ); ?>>
 			<h1>Page not found! 🫤</h1>
-			<p>It looks like nothing was found at this location. From time to time I will delete the odd post from this site. Problem is, I can't delete the corresponding post from the Fediverse, so that's why you might have ended up visiting a bloken link. Sorry about that.</p>
+			<p><b>It looks like nothing was found at this location. Sorry about that.</b></p>
+			
+			<p>From time to time I will delete the odd post from this site. Problem is, I can't delete the corresponding post from the Fediverse, so that's why you might have ended up visiting a bloken link. Sorry about that.</p>
+
 			<a class="brutal-shadow" href="/">Back to homepage</a>
 		</main>
 	</div>
